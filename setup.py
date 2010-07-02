3a5842d2cf61575d61b57b5c3de3debe6ad5e106
Thomas Ashelford <thomas@ether.com.au>
2010-07-02 17:40:40 +1000

    fixed setup.py bug

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='glamkit-commenttools',
-    version='0.5.0',
+    version='0.5.1',
     author='Julien Phalip',
     author_email='julien@interaction.net.au',
     description='Useful bits to spice up comments in your Django site.',
-    long_description=open('README.rst').read(),
     url='http://github.com/glamkit/glamkit-commenttools',
     packages=find_packages(),
     zip_safe=False,
